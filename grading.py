ec60ab08b04afaaab181790c08fb4428e8e10d20
bimonestle <bimonestle@yahoo.co.id>
2021-06-01 21:06:54 +0700

    Grading

---
+++ grading.py
@@ -0,0 +1,43 @@
+# HackerLand University has the following grading policy:
+# Every student receives a grade in the inclusive range from 0 to 100.
+# Any grade less than 40 is a failing grade.
+
+# Sam is a professor at the university and likes to round each student's grade according to these rules:
+# • If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+# • If the value of grade is less than 38, no rounding occurs as the result will still be a failing grade.
+
+# EXAMPLES
+# grade = 84 round to 85 (85 - 84 is less than 3)
+# grade = 29 do not round (result is less than 40)
+# grade = 57  do not round (60 - 57 is 3 or higher)
+
+# SAMPLE INPUT
+# 73
+# 67
+# 38
+# 33
+# Output
+# 75
+# 67
+# 40
+# 33
+
+def gradingStudents(grades):
+    # Write your code here
+    for idx, grade in enumerate(grades):
+        if grade < 38:
+            continue
+        modRes = grade % 5
+        print('grade: {}, % 5: {}'.format(grade, modRes))
+        if modRes != 0:
+            gap = 5 - modRes
+            print(gap)
+            if gap < 3:
+                roundUp = grade + gap
+                print(roundUp)
+                grades[idx] = roundUp
+    print(grades)
+    return grades
+
+scores = [73, 67, 38, 33]
+gradingStudents(scores)
