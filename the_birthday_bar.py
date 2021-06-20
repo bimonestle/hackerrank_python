4fb2d9ec013492d54b591279cd6c1d95b668f684
bimonestle <bimonestle@yahoo.co.id>
2021-06-20 22:00:18 +0700

    add new lines of code. 4/14 test cases failed

--- the_birthday_bar.py
+++ the_birthday_bar.py
@@ -1,15 +1,16 @@
 def birthday(s: list, d: int, m: int) -> int:
     count = 0
-    calc = s[0]
+    # calc = s[0]
     month = m - 1
 
     if len(s) == 1:
         if s[0] == d:
             count += 1
 
-    for i in range(1, len(s)):
+    for i in range(0, len(s)):
         print('idx', i)
-        for j in range(i, len(s)):
+        calc = s[i]
+        for j in range(i+1, len(s)):
             print('jdx', j)
             calc += s[j]
             print('month:', month)
@@ -18,7 +19,7 @@ def birthday(s: list, d: int, m: int) -> int:
             if month < 0:
                 month = m - 1
                 break
-            
+
             if calc == d:
                 # calc = s[i]
                 count += 1
@@ -28,21 +29,33 @@ def birthday(s: list, d: int, m: int) -> int:
                 # calc = s[i]
                 # month = m - 1
                 break
-        calc = s[i]
+        # calc = s[i]
         month = m - 1
+
+    if count > m:
+        # print('count:', count)
+        # print('remainder', (len(s) % m))
+        count = count - (len(s) % m)
     print(count)
     return count
 
 # arr = [1, 2, 1, 3, 2]
 # date = 3
 # month = 2
-# arr = [2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1]
-# date = 18
-# month = 7
+# # 2
+
+arr = [2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1]
+date = 18
+month = 7
+# 3
+
 # arr = [1, 1, 1, 1, 1, 1]
 # date = 3
 # month = 2
-arr = [2, 2, 2, 1, 3, 2, 2, 3, 3, 1, 4, 1, 3, 2, 2, 1, 2, 2, 4, 2, 2, 3, 5, 3, 4, 3, 2, 1, 4, 5, 4]
-date = 10
-month = 4
+# # 0
+
+# arr = [2, 2, 2, 1, 3, 2, 2, 3, 3, 1, 4, 1, 3, 2, 2, 1, 2, 2, 4, 2, 2, 3, 5, 3, 4, 3, 2, 1, 4, 5, 4]
+# date = 10
+# month = 4
+# # 7
 birthday(arr,date,month)
