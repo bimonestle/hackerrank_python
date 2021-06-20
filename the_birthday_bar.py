e1ee8a2f57d55098469b2e066367723b4839b1b9
bimonestle <bimonestle@yahoo.co.id>
2021-06-20 21:19:47 +0700

    Refactor code. 7/14 test cases failed.

--- the_birthday_bar.py
+++ the_birthday_bar.py
@@ -12,30 +12,37 @@ def birthday(s: list, d: int, m: int) -> int:
         for j in range(i, len(s)):
             print('jdx', j)
             calc += s[j]
+            print('month:', month)
             print('calc', calc)
             month -= 1
             if month < 0:
                 month = m - 1
                 break
+            
             if calc == d:
-                calc = s[i]
+                # calc = s[i]
                 count += 1
-                month = m -1
+                # month = m -1
                 break
             elif calc > d:
-                calc = s[i]
-                month = m - 1
+                # calc = s[i]
+                # month = m - 1
                 break
+        calc = s[i]
+        month = m - 1
     print(count)
     return count
 
 # arr = [1, 2, 1, 3, 2]
 # date = 3
 # month = 2
-arr = [2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1]
-date = 18
-month = 7
+# arr = [2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1]
+# date = 18
+# month = 7
 # arr = [1, 1, 1, 1, 1, 1]
 # date = 3
 # month = 2
+arr = [2, 2, 2, 1, 3, 2, 2, 3, 3, 1, 4, 1, 3, 2, 2, 1, 2, 2, 4, 2, 2, 3, 5, 3, 4, 3, 2, 1, 4, 5, 4]
+date = 10
+month = 4
 birthday(arr,date,month)
