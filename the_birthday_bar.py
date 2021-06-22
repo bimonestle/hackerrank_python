9c0a4317a3170006ff5dc26f4ba94d0d88fcbb29
bimonestle <bimonestle@yahoo.co.id>
2021-06-22 22:42:01 +0700

    debugging

--- the_birthday_bar.py
+++ the_birthday_bar.py
@@ -1,4 +1,5 @@
 def birthday(s: list, d: int, m: int) -> int:
+    print("Length:", len(s))
     count = 0
     # calc = s[0]
     month = m - 1
@@ -9,13 +10,16 @@ def birthday(s: list, d: int, m: int) -> int:
 
     for i in range(0, len(s)):
         print('idx', i)
+        if i > len(s) - m - 1:
+            break
         calc = s[i]
         for j in range(i+1, len(s)):
             print('jdx', j)
             calc += s[j]
-            print('month:', month)
+            print('month before:', month)
             print('calc', calc)
             month -= 1
+            print('month after:', month)
             if month < 0:
                 month = m - 1
                 break
@@ -31,31 +35,36 @@ def birthday(s: list, d: int, m: int) -> int:
                 break
         # calc = s[i]
         month = m - 1
-
+    print("Count before:", count)
     if count > m:
-        # print('count:', count)
-        # print('remainder', (len(s) % m))
-        count = count - (len(s) % m)
-    print(count)
+        remainder = len(s) % m
+        # if remainder == 0:
+        #     count -= count
+        count = count - remainder
+    print("Count after:", count)
     return count
 
 # arr = [1, 2, 1, 3, 2]
 # date = 3
 # month = 2
+# # length 5
 # # 2
 
 arr = [2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1]
 date = 18
 month = 7
+# length 19
 # 3
 
 # arr = [1, 1, 1, 1, 1, 1]
 # date = 3
 # month = 2
+# # length 6
 # # 0
 
 # arr = [2, 2, 2, 1, 3, 2, 2, 3, 3, 1, 4, 1, 3, 2, 2, 1, 2, 2, 4, 2, 2, 3, 5, 3, 4, 3, 2, 1, 4, 5, 4]
 # date = 10
 # month = 4
+# # length 31
 # # 7
 birthday(arr,date,month)
