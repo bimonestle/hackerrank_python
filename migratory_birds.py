77783283234f5b20606eb754edc39c0ed952ab80
bimonestle <bimonestle@yahoo.co.id>
2021-06-28 22:56:20 +0700

    1/6 test cases failed

--- migratory_birds.py
+++ migratory_birds.py
@@ -2,7 +2,7 @@ def migratoryBirds(arr):
     seen = dict()
     count = 1
     arr.sort(reverse=True)
-
+    print('arr:', arr)
     for i in range(len(arr)):
         try:
             if arr[i] == arr[i+1]:
@@ -12,3 +12,29 @@ def migratoryBirds(arr):
                 count = 1
         except:
             break
+    
+    print(seen)
+    keyList = list(seen.keys())
+    valList = list(seen.values())
+    result = 0
+    tempVal = 0
+    print('key list:',keyList)
+    print('val list:',valList)
+    for i in range(len(valList)):
+        if valList[i] > tempVal:
+            print('key:', keyList[i])
+            tempVal = valList[i]
+            result = keyList[i]
+        elif valList[i] == tempVal:
+            print('key:', keyList[i])
+            tempVal = valList[i]
+            result = keyList[i]
+        else:
+            break
+        print('loop result:', result)
+    print('result:', result)
+    return result
+
+arr = [2, 4, 3, 2, 3, 1, 2, 1, 3, 3]
+arr = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]
+migratoryBirds(arr)
