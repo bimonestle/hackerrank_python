aedede6c98da97ddea1382c3c020866f14f9a8ff
bimonestle <bimonestle@yahoo.co.id>
2021-06-29 15:09:08 +0700

    completed

--- migratory_birds.py
+++ migratory_birds.py
@@ -14,27 +14,23 @@ def migratoryBirds(arr):
             break
     
     print(seen)
-    keyList = list(seen.keys())
-    valList = list(seen.values())
+    sortSeen = sorted(seen.items(), key=lambda x:x[1], reverse=True)
+    print(sortSeen)
     result = 0
     tempVal = 0
-    print('key list:',keyList)
-    print('val list:',valList)
-    for i in range(len(valList)):
-        if valList[i] > tempVal:
-            print('key:', keyList[i])
-            tempVal = valList[i]
-            result = keyList[i]
-        elif valList[i] == tempVal:
-            print('key:', keyList[i])
-            tempVal = valList[i]
-            result = keyList[i]
+    for i in sortSeen:
+        if i[1] > tempVal:
+            print('key:', i[0])
+            tempVal = i[1]
+            result = i[0]
+        elif i[1] == tempVal:
+            print('key:', i[0])
+            tempVal = i[1]
+            result = i[0]
         else:
             break
-        print('loop result:', result)
-    print('result:', result)
     return result
 
 arr = [2, 4, 3, 2, 3, 1, 2, 1, 3, 3]
-arr = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]
+# arr = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4]
 migratoryBirds(arr)
