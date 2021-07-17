cc662e58ea67586351f8bea7a069cd6943c23674
bimonestle <bimonestle@yahoo.co.id>
2021-07-17 12:31:36 +0700

    1/27 test cases failed

---
+++ drawing_book.py
@@ -0,0 +1,14 @@
+def pageCount(n, p):
+    # Write your code here
+    leftOpen = int(p/2)
+    rightOpen = int((n-p)/2)
+    
+    result = 0
+    if leftOpen < rightOpen:
+        result = leftOpen
+        print(leftOpen)
+    else:
+        result = rightOpen
+        print(rightOpen)
+    
+    return result
