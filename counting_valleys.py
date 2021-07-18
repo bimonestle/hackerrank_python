5c54d12e7e8e8b8da69eea017ea85efbc21c06b8
bimonestle <bimonestle@yahoo.co.id>
2021-07-18 22:15:18 +0700

    add problem background

--- counting_valleys.py
+++ counting_valleys.py
@@ -1,3 +1,36 @@
+# An avid hiker keeps meticulous records of their hikes.
+# During the last hike that took exactly 'step' steps,
+# for every step it was noted if it was an uphill, 'U',
+# or a downhill, 'D' step. Hikes always start and end at sea level,
+# and each step up or down represents a '1' unit change in altitude.
+# We define the following terms:
+# • A mountain is a sequence of consecutive steps above sea level,
+#   starting with a step up from sea level and ending with a step down to sea level.
+# • A valley is a sequence of consecutive steps below sea level,
+#   starting with a step down from sea level and ending with a step up to sea level.
+
+# Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+
+# Example
+# steps = 8 path = [DDUUUUDD]
+# The hiker first enters a valley '2' units deep. Then they climb out
+# and up onto a mountain '2' units high. Finally, the hiker returns to sea level and ends the hike.
+# Returns:
+# int: the number of valleys traversed
+
+# Sample Input
+# 8
+# UDDDUDUU
+# Output: 1
+# Explanation:
+# If we represent _ as sea level, a step up as /, and a step down as \, the hike can be drawn as:
+# _/\      _
+#    \    /
+#     \/\/
+
+# The hiker enters and leaves one valley.
+
+
 def countingValleys(steps: int, path: str) -> int:
     summ = 0
     valleys = 0
