0c73d104fbb2679a57c776a2cab156f2e7d44b23
bimonestle <bimonestle@yahoo.co.id>
2021-07-19 09:55:48 +0700

    add problem background

--- electronic_shop.py
+++ electronic_shop.py
@@ -1,3 +1,29 @@
+# A person wants to determine the most expensive computer keyboard and
+# USB drive that can be purchased with a give budget. Given price lists
+# for keyboards and USB drives and a budget, find the cost to buy them.
+# If it is not possible to buy both items, return '-1'.
+
+# Example
+# Budget = 60
+# Keyboards = [40, 50, 60]
+# Drives = [5, 8, 12]
+# The person can buy a '40 keyboard + 12 USB drives'
+# or a '50 keyboard + 8 USB drives'.
+# Choose the latter as the more expensive option
+# and return '58'
+
+# Sample 1
+# 10 2 3
+# 3 1
+# 5 2 8
+# return 9
+
+# Sample 2
+# 5 1 1
+# 4
+# 5
+# return -1
+
 def getMoneySpent(keyboards, drives, b):
     pair = 0
     for keyboard in keyboards:
