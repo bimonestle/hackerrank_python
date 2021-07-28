615b0048eb1f2fe81080b4419e68711b24bb72e0
bimonestle <bimonestle@yahoo.co.id>
2021-07-28 22:42:48 +0700

    walkthrough comment

--- forming_magic_square.py
+++ forming_magic_square.py
@@ -1,4 +1,6 @@
 def formingMagicSquare(s: list) -> int:
+    # find if there are duplicates in each list
+    # the sum for each element must be 15
     for idx, lstA in enumerate(s):
         if sum(lstA) == 15:
             continue
