0506f7fe0ea962a221a5b68c478a934f576e3bf4
bimonestle <bimonestle@yahoo.co.id>
2021-08-04 22:07:26 +0700

    convert 2D arr to 1D arr

--- forming_magic_square.py
+++ forming_magic_square.py
@@ -1,6 +1,18 @@
 def formingMagicSquare(s: list) -> int:
     # find if there are duplicates in each list
     # the sum for each element must be 15
-    for idx, lstA in enumerate(s):
-        if sum(lstA) == 15:
-            continue
+    print(s)
+    one_Dim_Arr = list()
+    for lstA in s:
+        for lstB in lstA:
+            # print(lstB)
+            one_Dim_Arr.append(lstB)
+
+    print(one_Dim_Arr)
+    # for idx, el in enumerate(one_Dim_Arr):
+    #     if one_Dim_Arr[idx] == el:
+    #         print(True)
+
+s = [[5, 3, 4], [1, 5, 8], [6, 4, 2]]
+
+formingMagicSquare(s)
