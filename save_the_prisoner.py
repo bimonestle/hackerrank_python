464ba5420cb2033a72d358684602a48d33ffc191
bimonestle <bimonestle@yahoo.co.id>
2022-09-04 11:33:35 +0700

    pass the sample tests

--- save_the_prisoner.py
+++ save_the_prisoner.py
@@ -1,8 +1,22 @@
+from distutils.log import warn
+
+
 def saveThePrisoner(prisoners, candies, start):
     remaining_steps = candies % prisoners
-    warning = start - 1 + remaining_steps
     
-    while warning > prisoners:
-      warning = (warning % prisoners)
+    # Get the warned_prisoner from
+    # the remeaining_steps condition
+    if remaining_steps == 0:
+        if start == 1:
+          warned_prisoner = prisoners
+        else:
+          warned_prisoner = start - 1
+    else:
+        warned_prisoner = start - 1 + remaining_steps
+
+    # Loop to eliminate the remaining value
+    # of the warned_prisoner calculation
+    while warned_prisoner > prisoners:
+        warned_prisoner = warned_prisoner % prisoners
     
-    return warning
+    return warned_prisoner
